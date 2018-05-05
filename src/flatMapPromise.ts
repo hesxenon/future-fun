afe25fd2ad4f06d45038cfe73789288c25d256dc
Daniel Potensky <daniel.potensky@gmail.com>
2018-05-05 23:57:08 +0200

    fixup renaming

--- src/flatMapPromise.ts
+++ src/flatMapPromise.ts
@@ -1,6 +1,6 @@
 import { CallFn, Operator, createOperator, Finish, Call, pipe } from '..'
 
-export function asyncFlatmap<In, Out> (fn: CallFn<In, Promise<Out>>, thisArg?: any): Operator<In, Out> {
+export function flatMapPromise<In, Out> (fn: CallFn<In, Promise<Out>>, thisArg?: any): Operator<In, Out> {
   return createOperator(fn, thisArg, (previous, finish) => {
     fn.call(thisArg, previous).then(finish)
   })
