095bb53f13c841ac08c3bc7c68d8d444123d5f8c
Daniel Potensky <daniel.potensky@gmail.com>
2018-05-06 00:54:46 +0200

    map to call with pipes is now possible

--- test/flatMap.spec.ts
+++ test/flatMap.spec.ts
@@ -2,12 +2,29 @@ import { ident, double } from './test.util'
 import { call } from '../src/call'
 import { flatMap } from '../src/flatMap'
 import { assert } from 'chai'
+import { map } from '..'
 
 describe('flatMap', () => {
   it('should be able to pass on the result of a nested call', done => {
     const c = call(ident, 1).pipe(flatMap(x => call(double, x)))
     c.then(x => {
       assert(x === 2)
+      done()
+    })
+  })
+
+  it('should be able to resolve mapped calls', done => {
+    const apiCall = (x: number) => x + ''
+    const parseResponse = parseInt
+    const c = call(x => x, 1)
+      .pipe(map(x => call(apiCall, x).pipe(map(parseResponse))))
+
+    c.exec(2, ({ previous: { fn } }) => {
+      assert(fn === apiCall)
+    })
+
+    c.pipe(flatMap(x => x)).then(x => {
+      assert(x === 1)
       done()
     })
   })
