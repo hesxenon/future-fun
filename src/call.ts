826289335f38417b708abf88d887af91beaa9dbf
Daniel Potensky <daniel.potensky@gmail.com>
2018-05-07 01:35:02 +0200

    always necessary for typing but can be 'undefined'

--- src/call.ts
+++ src/call.ts
@@ -1,7 +1,7 @@
 import { CallFn, Call, Operator } from './types'
 import { pipe } from './util'
 
-export function call<Resolve, In> (fn: CallFn<In, Resolve>, arg?: In, thisArg?: any): Call<Resolve, In, Resolve, undefined> {
+export function call<Resolve, In> (fn: CallFn<In, Resolve>, arg: In, thisArg?: any): Call<Resolve, In, Resolve, undefined> {
   const exec = (arg?: In) => fn.call(thisArg, arg)
   return {
     fn, arg, previous: undefined, thisArg,
