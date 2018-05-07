2928ec9aaadede4dbe90671e1683c5f069f48f4c
Daniel Potensky <daniel.potensky@gmail.com>
2018-05-07 13:25:50 +0200

    out first and naming

--- src/flatMap.ts
+++ src/flatMap.ts
@@ -1,7 +1,7 @@
 import { CallFn, Call, Operator, ResolveOf } from './types'
 import { createOperator } from './util'
 
-export function flatMap<In = any, Out extends Call = any> (mapFn: CallFn<In, Out>, thisArg?: any): Operator<ResolveOf<Out>, In, Out> {
+export function flatMap<Resolve extends Call = any, In = any> (mapFn: CallFn<In, Resolve>, thisArg?: any): Operator<ResolveOf<Resolve>, In, Resolve> {
   return createOperator(mapFn, thisArg, (previous, finish) => {
     (mapFn.call(thisArg, previous) as ReturnType<typeof mapFn>).exec(finish)
   })
