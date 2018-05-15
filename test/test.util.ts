cce183494ee47b4b25a7f011a12f6cba76b68204
Daniel Potensky <daniel.potensky@gmail.com>
2018-05-15 03:45:08 +0200

    name confict with js toString

--- test/test.util.ts
+++ test/test.util.ts
@@ -1,4 +1,4 @@
 export const ident = (x: number) => x
 export const double = (x: number) => x * 2
 export const increment = (x: number) => x + 1
-export const toString = (x: number) => x + ''
+export const stringify = (x: number) => x + ''
