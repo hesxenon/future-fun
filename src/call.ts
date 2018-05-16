bb75f7512f7626a7fbb2f6106d559f6ca05d742c
Daniel Potensky <daniel.potensky@gmail.com>
2018-05-16 17:41:33 +0200

    added interface for Call.all

--- src/call.ts
+++ src/call.ts
@@ -1,4 +1,4 @@
-import { ICallMonad, InOf, OutOf, ILift } from '..'
+import { ICallMonad, ILift, IAll } from '..'
 
 /**
  * @deprecated - use Call.of instead
@@ -25,16 +25,9 @@ export function Call<In, Out> (fn: (arg: In) => Out, arg: In, thisArg?: any): IC
 
 export namespace Call {
   export const of: ILift = Call
-  export const all = _all
+  export const all: IAll = _all
 }
 
-type M = ICallMonad<any, any>
-
-function _all<M1 extends M> (m1: M1): ICallMonad<[M1], OutOf<M1>>
-function _all<M1 extends M, M2 extends M> (m1: M1, m2: M2): ICallMonad<[M1, M2], [OutOf<M1>, OutOf<M2>]>
-function _all<M1 extends M, M2 extends M, M3 extends M> (m1: M1, m2: M2, m3: M3): ICallMonad<[M1, M2, M3], [OutOf<M1>, OutOf<M2>, OutOf<M3>]>
-function _all<M1 extends M, M2 extends M, M3 extends M, M4 extends M> (m1: M1, m2: M2, m3: M3, m4: M4): ICallMonad<[M1, M2, M3, M4], [OutOf<M1>, OutOf<M2>, OutOf<M3>, OutOf<M4>]>
-function _all<M1 extends M, M2 extends M, M3 extends M, M4 extends M, M5 extends M> (m1: M1, m2: M2, m3: M3, m4: M4, m5: M5): ICallMonad<[M1, M2, M3, M4, M5], [OutOf<M1>, OutOf<M2>, OutOf<M3>, OutOf<M4>, OutOf<M5>]>
-function _all (...calls: ICallMonad<any, any>[]): ICallMonad<any, any[]> {
+function _all (...calls: ICallMonad<any, any>[]): ICallMonad<any, any> {
   return Call.of(calls => calls.map(call => call.valueOf()), calls)
 }
--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,30 @@
 export interface ICallMonad<In, Out> extends ICall<In, Out> {
-  chain: <Next extends ICallMonad<any, any>, Instance extends this>(this: Instance, f: (arg: Out) => Next) => IChainedCallMonad<Instance, OutOf<Next>, Next>,
+  chain: <Next extends M, Instance extends this>(this: Instance, f: (arg: Out) => Next) => IChainedCallMonad<Instance, OutOf<Next>, Next>,
   map: <Next, Instance extends this>(this: Instance, f: MapFn<Out, Next>) => IMappedCallMonad<Instance, Next>,
   valueOf: () => Out
 }
 
-export interface IChainedCallMonad<Instance extends ICallMonad<any, any>, Out, Next> extends ICallMonad<Instance, Out> {
+export interface IChainedCallMonad<Instance extends M, Out, Next> extends ICallMonad<Instance, Out> {
   chainFn: MapFn<OutOf<Instance>, Next>
 }
 
-export interface IMappedCallMonad<Instance extends ICallMonad<any, any>, Out> extends ICallMonad<Instance, Out> {
+export interface IMappedCallMonad<Instance extends M, Out> extends ICallMonad<Instance, Out> {
   mapFn: MapFn<OutOf<Instance>, Out>
 }
 
 export interface ILift {
   <In, Out>(fn: (arg: In) => Out, arg: In): ICallMonad<In, Out>
 }
+
+export interface IAll {
+  <M1 extends M>(m1: M1): ICallMonad<[M1], [OutOf<M1>]>
+  <M1 extends M, M2 extends M>(m1: M1, m2: M2): ICallMonad<[M1, M2], [OutOf<M1>, OutOf<M2>]>
+  <M1 extends M, M2 extends M, M3 extends M>(m1: M1, m2: M2, m3: M3): ICallMonad<[M1, M2, M3], [OutOf<M1>, OutOf<M2>, OutOf<M3>]>
+  <M1 extends M, M2 extends M, M3 extends M, M4 extends M>(m1: M1, m2: M2, m3: M3, m4: M4): ICallMonad<[M1, M2, M3, M4], [OutOf<M1>, OutOf<M2>, OutOf<M3>, OutOf<M4>]>
+  <M1 extends M, M2 extends M, M3 extends M, M4 extends M, M5 extends M>(m1: M1, m2: M2, m3: M3, m4: M4, m5: M5): ICallMonad<[M1, M2, M3, M4, M5], [OutOf<M1>, OutOf<M2>, OutOf<M3>, OutOf<M4>, OutOf<M5>]>
+}
+
+export type M = ICallMonad<any, any>
 
 export type InOf<C> = C extends ICall<infer In, infer Out> ? In : any
 export type OutOf<C> = C extends ICall<infer In, infer Out> ? Out : any
