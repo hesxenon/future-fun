2fdfe58910d6ea2ae83f5a1d4700d17a1a16188a
Daniel Potensky <daniel.potensky@gmail.com>
2018-05-16 17:52:59 +0200

    fixup

--- src/types.ts
+++ src/types.ts
@@ -22,6 +22,7 @@ export interface IAll {
   <M1 extends M, M2 extends M, M3 extends M>(m1: M1, m2: M2, m3: M3): ICallMonad<[M1, M2, M3], [OutOf<M1>, OutOf<M2>, OutOf<M3>]>
   <M1 extends M, M2 extends M, M3 extends M, M4 extends M>(m1: M1, m2: M2, m3: M3, m4: M4): ICallMonad<[M1, M2, M3, M4], [OutOf<M1>, OutOf<M2>, OutOf<M3>, OutOf<M4>]>
   <M1 extends M, M2 extends M, M3 extends M, M4 extends M, M5 extends M>(m1: M1, m2: M2, m3: M3, m4: M4, m5: M5): ICallMonad<[M1, M2, M3, M4, M5], [OutOf<M1>, OutOf<M2>, OutOf<M3>, OutOf<M4>, OutOf<M5>]>
+  (...calls: ICallMonad<any, any>[]): ICallMonad<any, any[]>
 }
 
 export type M = ICallMonad<any, any>
