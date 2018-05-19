b4b63253f7fc3e267a020ebfa0dbd9d6f64ae2f3
Daniel Potensky <daniel.potensky@gmail.com>
2018-05-19 14:29:22 +0200

    added nullaryfunction type

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export interface ICallMonad<In, Out> {
   fn: UnaryFunction<In, Out>
   with: <Instance extends this>(this: Instance, arg: In) => IExecutable<Instance>
   map: <Instance extends this, Next>(this: Instance, morphism: UnaryFunction<Out, Next>) => IPipedCallMonad<InOf<Instance>, Next, UnaryFunction<OutOf<Instance>, Next>, Instance>
-  chain: <Instance extends this, Next extends M>(this: Instance, next: Next) => IPipedCallMonad<InOf<Instance>, OutOf<Next>, UnaryFunction<OutOf<Instance>, Next>, Instance>
+  chain: <Instance extends this, Next extends M>(this: Instance, next: Next) => IPipedCallMonad<InOf<Instance>, OutOf<Next>, NullaryFunction<Next>, Instance>
 }
 
 export interface IExecutable<Instance extends M> {
@@ -30,6 +30,10 @@ export interface IAll {
   <M1 extends M, M2 extends M, M3 extends M, M4 extends M>(m1: M1, m2: M2, m3: M3, m4: M4): ICallMonad<[InOf<M1>, InOf<M2>, InOf<M3>, InOf<M4>], [OutOf<M1>, OutOf<M2>, OutOf<M3>, OutOf<M4>]>
   <M1 extends M, M2 extends M, M3 extends M, M4 extends M, M5 extends M>(m1: M1, m2: M2, m3: M3, m4: M4, m5: M5): ICallMonad<[InOf<M1>, InOf<M2>, InOf<M3>, InOf<M4>, InOf<M5>], [OutOf<M1>, OutOf<M2>, OutOf<M3>, OutOf<M4>, OutOf<M5>]>
   (...calls: M[]): ICallMonad<any[], any[]>
+}
+
+export interface NullaryFunction<Out> {
+  (): Out
 }
 
 export interface UnaryFunction<In, Out> {
