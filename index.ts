95744c0c79eef8767ac08d22f7f1074ebf94af92
Daniel Potensky <daniel.potensky@gmail.com>
2018-05-19 03:54:43 +0200

    renaming

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export * from './src/call'
-export { ICallMonad, UnaryFunction, InOf, OutOf, IBinaryOperator } from './src/types'
+export { ICallMonad, UnaryFunction, InOf, OutOf, IOperator } from './src/types'
 
 export { mapPromise } from './src/operators/mapPromise'
