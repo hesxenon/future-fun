b492b75f59dd1827cae837156365229c0a7755fe
Daniel Potensky <daniel.potensky@gmail.com>
2018-05-19 14:10:33 +0200

    removing for now, too complicated

--- src/operators/flatMapTo.ts
+++
@@ -1,7 +0,0 @@
-import { M, IOperator, OutOf, InOf } from '../types'
-import { createOperator } from './util'
-
-export const flatMapTo: <To extends M>(call: To) => IOperator<any, OutOf<To>, () => To> = (call) => Object.assign(
-  (result: InOf<typeof call>) => call.with(result).exec(),
-  { morphism: () => call }
-)
